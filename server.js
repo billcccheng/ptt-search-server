3110999fed25679845c9a98228dc1497fda6c4b1
Bill Cheng <chun-chan.cheng@viasat.com>
2017-05-12 16:50:55 -0500

    migrating

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 let express = require("express");
 let app = express()
 let fs = require("fs");
+let promise = require("promise");
+let objectValues = require("object-values");
+
 let allowCrossDomain = function(req, res, next) {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
@@ -8,44 +11,47 @@ let allowCrossDomain = function(req, res, next) {
 
   // intercept OPTIONS method
   if ('OPTIONS' == req.method) {
-  res.send(200);
+    res.send(200);
   }
   else {
-  next();
+    next();
   }
 };
 
 app.use(allowCrossDomain);
 
-app.get('/api',function(req, res){
-  res.send("TEST")
-  //fs.readFile(__dirname + '/states/' + req.query.state + '.json', function(err, data){
-    //if(err){
-      //res.send("No such State");
-      //return;
-    //}
-    //console.log(req.query);
-    //jsonData = JSON.parse(data.toString());
-    //let response = "No such county";
-    //for(x in jsonData){
-      //if(jsonData[x]['County Name'] === req.query.county){
-        //let coordinates = getCoordinates(jsonData[x].geometry);
-        //let countyBoundary = new Object();
-        //countyBoundary.state = req.query.state;
-        //countyBoundary.county = req.query.county;
-        //countyBoundary.shape = coordinates;
-        //response = countyBoundary;
-        //break;
-      //}
-    //}
-    //res.setHeader('Content-Type', 'application/json');
-    //res.json(response);
-  //})
+app.get('/api', (req, res) => {
+  console.log(req.query.inputs.split(","));
+  let params = req.query.inputs.split(",");
+
+  let dataObj = getDataHits(params); 
+
+  res.setHeader('Content-Type', 'application/json');
+  res.sendStatus(200);
+  //res.json(response);
 });
 
-console.log("TEST");
+
+function getDataHits(queries){
+  let dataHits = [];
+  for(let i = 0; i < 60; i++){
+    let file = fs.readFileSync(__dirname + "/data/data-" + i + ".json", "utf-8");
+    file = JSON.parse(file);
+    file.forEach(dataObj => {
+      data = objectValues(dataObj);
+      data = data.toString().toLowerCase();
+      let hit = queries.every( query => data.includes(query.toLowerCase()));
+      if(hit)
+        console.log(dataObj.link);
+    })
+  }
+  console.log("FINISHED");
+}
+
 let server = app.listen(process.env.PORT||8080, function(){
   let port = server.address().port;
   console.log("App running on port %s ",port);
 });
+
+
 
