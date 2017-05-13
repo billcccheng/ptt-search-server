bcebdada698d60be1026c1f52c40c02efda94c5d
Bill <iamkg05@gmail.com>
2017-05-13 10:41:53 -0500

    data updates

--- server.js
+++ server.js
@@ -21,34 +21,51 @@ let allowCrossDomain = function(req, res, next) {
 app.use(allowCrossDomain);
 
 app.get('/api', (req, res) => {
-  console.log(req.query.inputs.split(","));
+  let boardName = req.query.board;
   let params = req.query.inputs.split(",");
-
-  let dataObj = getDataHits(params); 
+  let response = getDataHits(boardName, params); 
 
   res.setHeader('Content-Type', 'application/json');
-  res.sendStatus(200);
-  //res.json(response);
+  res.json(response);
 });
 
 
-function getDataHits(queries){
-  let dataHits = [];
-  for(let i = 0; i < 60; i++){
-    let file = fs.readFileSync(__dirname + "/data/data-" + i + ".json", "utf-8");
-    file = JSON.parse(file);
-    file.forEach(dataObj => {
-      data = objectValues(dataObj);
-      data = data.toString().toLowerCase();
-      let hit = queries.every( query => data.includes(query.toLowerCase()));
-      if(hit)
-        console.log(dataObj.link);
-    })
+function isDuplicate(firstTimeData, link){
+  if(firstTimeData.has(link)){
+    return true;
   }
-  console.log("FINISHED");
+  firstTimeData.add(link);
+  return false;
 }
 
-let server = app.listen(process.env.PORT||8080, function(){
+function getDataHits(boardName, queries){
+  let contentHitsObject = {};
+  let firstTimeData = new Set();
+  for(let i = 0; i < 60; i++){
+    let file = fs.readFileSync(__dirname + "/" + boardName + "/data-" + i + ".json", "utf-8");
+    file = JSON.parse(file);
+    file.forEach(dataObj => {
+      let dataObjValue = objectValues(dataObj).toString().toLowerCase();
+      let hit = queries.every( query => dataObjValue.includes(query.toLowerCase()));
+      if(hit && !isDuplicate(firstTimeData, dataObj.link)){
+        let date = dataObj.日期.split(" ");
+        let year = date[date.length - 1];
+        if(/^\d+$/.test(year)){
+          if(!(year in contentHitsObject)){
+            contentHitsObject[year] = [];
+          }
+          let contentHit = {};
+          contentHit.title = dataObj.標題;
+          contentHit.link = dataObj.link;
+          contentHitsObject[year].push(contentHit);
+        }
+      }
+    })
+  }
+  return contentHitsObject;
+}
+
+let server = app.listen(process.env.PORT||8081, function(){
   let port = server.address().port;
   console.log("App running on port %s ",port);
 });
